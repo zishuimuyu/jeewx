001f4254eadda3e16e017e74d79550f9934b5156
zhangdaiscott <zhangdaiscott@163.com>
2014-07-07 16:12:53 +0800

    jeewx 目录调整，代码修正
    jeewx 目录调整，代码修正

---
+++ src/weixin/guanjia/account/service/WeixinAccountServiceI.java
@@ -0,0 +1,40 @@
+package weixin.guanjia.account.service;
+
+import org.jeecgframework.core.common.service.CommonService;
+
+import weixin.guanjia.account.entity.WeixinAccountEntity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface WeixinAccountServiceI extends CommonService{
+	@Override
+ 	public <T> void delete(T entity);
+ 	@Override
+ 	public <T> Serializable save(T entity);
+ 	@Override
+ 	public <T> void saveOrUpdate(T entity);
+ 	
+ 	/**
+	 * 默认按钮-sql增强-新增操作
+	 * @param id
+	 * @return
+	 */
+ 	public boolean doAddSql(WeixinAccountEntity t);
+ 	/**
+	 * 默认按钮-sql增强-更新操作
+	 * @param id
+	 * @return
+	 */
+ 	public boolean doUpdateSql(WeixinAccountEntity t);
+ 	/**
+	 * 默认按钮-sql增强-删除操作
+	 * @param id
+	 * @return
+	 */
+ 	public boolean doDelSql(WeixinAccountEntity t);
+ 	
+ 	public String getAccessToken();
+ 	public WeixinAccountEntity findLoginWeixinAccount();
+ 	public List<WeixinAccountEntity> findByUsername(String username);
+}
